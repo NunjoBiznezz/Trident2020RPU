3b2867f2a0e45a2f11ed303dda6b69ecd87c3189
nunjobiznezz <doug.bercot@gmail.com>
2026-07-29 09:39:03 -0400

    Native sounds

--- include/AudioHandler.h
+++ include/AudioHandler.h
@@ -2,6 +2,17 @@
 
 #include <stdint.h>
 
+/*********************************************************************
+    Native SB100 Effect Definitions
+*********************************************************************/
+constexpr uint8_t SOUND_NATIVE_NONE = 0x00;           // This turns sounds off
+constexpr uint8_t SOUND_NATIVE_TEN = 0x01;            // This plays the ten sound
+constexpr uint8_t SOUND_NATIVE_ONE_HUNDRED = 0x02;    // This plays the one hundred sound
+constexpr uint8_t SOUND_NATIVE_ONE_THOUSAND = 0x04;   // This plays the one thousand sound
+constexpr uint8_t SOUND_NATIVE_TEN_THOUSAND = 0x08;   // This plays the ten thousand sound
+constexpr uint8_t SOUND_NATIVE_ADD_BONUS = 0x10;      // This plays the add bonus sound
+constexpr uint8_t SOUND_NATIVE_POP_BUMPER = 0x20;     // This plays the pop bumper sound
+
 struct SoundEntry {
    uint16_t soundIndex;
    unsigned long playTime;
